e838cbeba88cecfe403ec6f231d05d1f9c8fd4a8
EsraaRedaElgiz <esraaelgiz@gmail.com>
2024-02-19 20:00:28 +0200

    env

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductStaticService {
-  baseURL: string = 'https://shoes-i0ks.onrender.com/products';
+  baseURL: string = environment.apiUrl;
 
   constructor(public http: HttpClient) {}
   //read
